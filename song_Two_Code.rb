c2a6a0029b56ee811b61ba4a880a1c63eff5b9d1
its-miss-arii <arianna.m.rodriguez0809@gmail.com>
2026-04-09 11:38:00 -0700

    Update song_Two_Code.rb

--- song_Two_Code.rb
+++ song_Two_Code.rb
@@ -10,18 +10,14 @@ miss_me = "C:/Users/arianna_rodriguez/Documents/Audacity/dont smile vocals.wav"
 define :measures do
   use_synth :pretty_bell
   #Measure 1
-  play :g4, amp: 0.5
-  sleep 0.5
-  play :g4, amp: 0.5
-  sleep 0.5
-  play :g4, amp: 0.5
-  sleep 0.5
-  play :b4
-  sleep 0.5
-  play :b4
-  sleep 0.5
-  play :b4
-  sleep 0.5
+  3.times do
+    play :g4, amp: 0.5
+    sleep 0.5
+  end
+  3.times do
+    play :b4
+    sleep 0.5
+  end
   play :e5; play :b4, amp: 2
   sleep 0.55
   play :b4
@@ -29,78 +25,71 @@ define :measures do
   #Measure 2
   play :a4
   sleep 0.5
-  play :b4
-  sleep 0.5
-  play :b4
-  sleep 0.5
-  play :b4
-  sleep 0.5
-  play :b4
-  sleep 0.5
-  play :b4
-  sleep 0.5
-  play :b4
-  sleep 0.5
-  play :c5
-  sleep 0.5
-  play :c5
-  sleep 0.5
+  5.times do
+    play :b4
+    sleep 0.5
+  end
+  2.times do
+    play :c5
+    sleep 0.5
+  end
   #Measure 3
-  play :b4
-  sleep 0.5
-  play :b4
-  sleep 0.5
-  play :b4
-  sleep 0.5
-  play :e5
-  sleep 0.5
-  play :e5
-  sleep 0.5
-  play :e5
-  sleep 0.5
-  play :e5
-  sleep 0.5
-  play :e5
-  sleep 0.5
+  3.times do
+    play :b4
+    sleep 0.5
+  end
+  5.times do
+    play :e5
+    sleep 0.5
+  end
   #Measure 4
-  play :a4
-  sleep 0.5
-  play :a4
-  sleep 0.5
-  play :a4
-  sleep 0.5
-  play :a4
-  sleep 0.5
-  play :a4
-  sleep 0.5
+  5.times do
+    play :a4
+    sleep 0.5
+  end
   play :g4
   sleep 0.5
-  play :a4
-  sleep 0.5
+  2.times do
+    play :a4, amp: 0.5
+    sleep 0.5
+  end
   play :a4, amp: 0.5
-  sleep 0.5
-  play :a4, amp: 0.5
-  sleep 0.5
+  sleep 0.4
 end
 
 define :vocals do
-  sample miss_me, start: 0, finish: 0.25, amp: 0.125
-  sleep 6.437263794406651/4
-  sample miss_me, start: 0.25, finish: 0.5, amp: 0.25
-  sleep 6.437263794406651/4
-  sample miss_me, start: 0.5, finish: 0.75, amp: 0.50
-  sleep 6.437263794406651/4
-  sample miss_me, start: 0.75, finish: 1, amp: 0.25
-  sleep 6.437263794406651/4
+  sample miss_me, start: 0, finish: 0.1, amp: 0.125
+  sleep 6.437263794406651/10
+  sample miss_me, start: 0.1, finish: 0.2, amp: 0.35
+  sleep 6.437263794406651/10
+  sample miss_me, start: 0.2, finish: 0.3, amp: 0.7
+  sleep 6.437263794406651/10
+  sample miss_me, start: 0.3, finish: 0.4, amp: 1.1
+  sleep 6.437263794406651/10
+  sample miss_me, start: 0.4, finish: 0.5, amp: 0.7
+  sleep 6.437263794406651/10
+  sample miss_me, start: 0.5, finish: 0.6, amp: 0.55
+  sleep 6.437263794406651/10
+  sample miss_me, start: 0.6, finish: 0.7, amp: 0.3
+  sleep 6.437263794406651/10
+  sample miss_me, start: 0.7, finish: 0.8, amp: 0.2
+  sleep 6.437263794406651/10
+  sample miss_me, start: 0.8, finish: 0.9, amp: 0.125
+  sleep 6.437263794406651/10
+  sample miss_me, start: 0.9, finish: 1, amp: 0.025
+  sleep 6.437263794406651/10
+  
 end
+
 #--------------MUSIC STARTS HERE--------------#
+
 
 #Transition
 sample :loop_drone_g_97, amp: 0.75
 sleep 8
 sample :drum_cymbal_soft
 sleep 1
-#Layer 2
+#Layer 2-main melody begins-Don't Smile beat
 live_loop :second do
   3.times do
     measures
@@ -109,7 +98,7 @@ live_loop :second do
   stop
 end
 sleep 6
-#Layer 3
+#Layer 3-Drums enter-need to sync w/ notes
 live_loop :drums do
   6.times do
     7.times do
@@ -121,15 +110,15 @@ live_loop :drums do
   end
   stop
 end
-
 sleep 21
+#Layer 4-vocal sample-miss_me-make louder
 
-#Layer 4
 3.times do
+  print ("vocals")
   vocals
-  sleep 2
+  sleep 3
 end
-
+#Outro sample comes in to end song
 live_loop :outro do
   1.times do
     sample :ambi_glass_hum, amp: 2
